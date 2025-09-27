72e1e41b2b97123cf61e5aa8e766e05d47098bc4
Hunain Sheikh <hunainsheikh5656@gmail.com>
2025-09-27 15:13:13 +0500

    Update Task01.java

--- src/Lab-o6/Task01.java
+++ src/Lab-o6/Task01.java
@@ -15,7 +15,105 @@ class linkedList {
 
     void insert(char data) {
         Node n = new Node(data);
-        if(head == null) {
+        if(head == null) {class LLNode {
+    char value;
+    LLNode nextNode;
+    
+    LLNode(char value) {
+        this.value = value;
+        this.nextNode = null;
+    }
+}
+
+class CharLinkedList {
+    LLNode first = null;
+    LLNode last = null;
+
+    void add(char value) {
+        LLNode newNode = new LLNode(value);
+        if(first == null) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.nextNode = newNode;
+            last = newNode;
+        }
+    }
+
+    void addAfter(char target, char newValue) {
+        LLNode currentNode = first;
+        while(currentNode != null) {
+            if(currentNode.value == target) {
+                LLNode newNode = new LLNode(newValue);
+                newNode.nextNode = currentNode.nextNode;
+                currentNode.nextNode = newNode;
+            }
+            currentNode = currentNode.nextNode;
+        }
+    }
+
+    void addAtStart(char value){
+        LLNode newNode = new LLNode(value);
+        newNode.nextNode = first;
+        first = newNode;
+    }
+
+    void find(char value) {
+        LLNode currentNode = first;
+        char pos = 0;
+        while(currentNode != null) {
+            if(currentNode.value == value) {
+                System.out.println("Item found: " + value + " at index " + pos);
+            }
+            currentNode = currentNode.nextNode;
+            pos++;
+            if(currentNode == null) {
+                System.out.println("Item "+ value +" not found: ");
+            }
+        }
+    }
+
+    void show() {
+        LLNode currentNode = first;
+        while(currentNode != null) {
+            System.out.print(currentNode.value + " -> ");
+            currentNode = currentNode.nextNode;
+        }
+        System.out.println("null");
+    }
+
+    void reverseList() {
+        LLNode prevNode = null;
+        LLNode currentNode = first;
+        LLNode nextNode = null;
+        last = first; // Update last to the original first
+        while(currentNode != null) {
+            nextNode = currentNode.nextNode;
+            currentNode.nextNode = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        first = prevNode; // Update first to the new front
+    }
+}
+
+public class Task01 {
+    public static void main(String[] args) {
+        CharLinkedList list = new CharLinkedList();
+        list.add('F');
+        list.add('D');
+        list.add('A');
+
+        list.show();
+
+        list.addAfter('D', 'E');
+        list.show();
+
+        list.addAtStart('B');
+        list.show();
+    }
+}
+
             head = n;
             tail = n;
         } else {
