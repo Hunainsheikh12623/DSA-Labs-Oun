f98c536e8cf95dc1eae640c798c431a1974ff146
Hunain Sheikh <hunainsheikh5656@gmail.com>
2025-09-27 15:03:14 +0500

    Update Task0401.java

--- Lab-04/Task0401.java
+++ Lab-04/Task0401.java
@@ -4,51 +4,50 @@ import java.util.*;
 public class Task0401 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        float[] arr = new float[20];
+        float[] numbers = new float[20];
 
-        for (int i = 0; i < arr.length; i++)  {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            arr[i] = sc.nextFloat();
+        for (int index = 0; index < numbers.length; index++)  {
+            System.out.print("Enter number " + (index + 1) + ": ");
+            numbers[index] = input.nextFloat();
         }
 
-        String userInput;
+        String order;
         System.out.println("Enter the sorting order (asc/desc): ");
-        userInput = sc.next();
+        order = input.next();
 
-
-        sc.close();
+        input.close();
         System.out.println("You entered the following numbers:");
 
         //bubble sort
-      if(userInput.equals("asc")) {
-          for (int i = 0; i < arr.length - 1; i++) {
-                for (int j = 0; j < arr.length - i - 1; j++) {
-                    if (arr[j] > arr[j + 1]) {
-                        float temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
+        if(order.equals("asc")) {
+            for (int index = 0; index < numbers.length - 1; index++) {
+                for (int j = 0; j < numbers.length - index - 1; j++) {
+                    if (numbers[j] > numbers[j + 1]) {
+                        float swap = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = swap;
                     }
                 }
             }
-            for (float num : arr) {
-                System.out.print(num + " ");
+            for (float value : numbers) {
+                System.out.print(value + " ");
             }
         }
 
-        else if(userInput.equals("desc")) {
-            for (int i = 0; i < arr.length - 1; i++) {
-                for (int j = 0; j < arr.length - i - 1; j++) {
-                    if (arr[j] < arr[j + 1]) {
-                        float temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
+        else if(order.equals("desc")) {
+            for (int index = 0; index < numbers.length - 1; index++) {
+                for (int j = 0; j < numbers.length - index - 1; j++) {
+                    if (numbers[j] < numbers[j + 1]) {
+                        float swap = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = swap;
                     }
                 }
             }
-            for (float num : arr) {
-                System.out.print(num + " ");
+            for (float value : numbers) {
+                System.out.print(value + " ");
             }
         }
 
